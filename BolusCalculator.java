7e4ef7b7cd98e0c97a841ac0d74443d66cc70b66
Richard <104224168+richardsie25@users.noreply.github.com>
2024-01-27 19:58:44 -0800

    Update BolusCalculator.java

--- BolusCalculator.java
+++ BolusCalculator.java
@@ -1,23 +1,36 @@
 package com.example.diediabetes;
-
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class BolusCalculator extends Activity {
 
     private EditText editTextCarbRatio, editTextGlucoseReading;
     private double totalCarbsConsumed;
-    private CheckBox checkBoxCondition1, checkBoxCondition2, checkBoxCondition3, checkBoxCondition4, checkBoxCondition5;
+    private double totalBolus;
+    private CheckBox checkBoxCondition1, checkBoxCondition2, checkBoxCondition3, checkBoxCondition4;
     private TextView textViewResult;
+    private Spinner insulinTimingSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +47,11 @@ public class BolusCalculator extends Activity {
         checkBoxCondition3 = findViewById(R.id.checkBoxCondition3);
         checkBoxCondition4 = findViewById(R.id.checkBoxCondition4);
 
+        insulinTimingSpinner = findViewById(R.id.insulinTimingSpinner);
+        setupSpinner();
+
         Button buttonReturn = findViewById(R.id.buttonReturn);
-        buttonReturn.setOnClickListener(v -> {
-            finish();
-        });
+        buttonReturn.setOnClickListener(v -> finish());
 
         TextWatcher textWatcher = new TextWatcher() {
             @Override
@@ -53,28 +67,69 @@ public class BolusCalculator extends Activity {
         editTextCarbRatio.addTextChangedListener(textWatcher);
         editTextGlucoseReading.addTextChangedListener(textWatcher);
 
+        Button buttonHome = findViewById(R.id.buttonHome);
+        buttonHome.setOnClickListener(v -> {
+            /*Intent intent = new Intent(CarbCalculator.this, logsActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            finish();*/
+        });
+
         Button buttonDone = findViewById(R.id.buttonDone);
         buttonDone.setOnClickListener(v -> {
-            if (areFieldsFilled()) {
+            if (areFieldsFilled() && insulinTimingSpinner.getSelectedItemPosition() != 0) {
+                /*Intent sendDataIntent = new Intent(BolusCalculator.this, logsActivity.class);
+
+                sendDataIntent.putExtra("CarbsConsumed", totalCarbsConsumed);
+                sendDataIntent.putExtra("GlucoseReading", Double.parseDouble(editTextGlucoseReading.getText().toString()));
+                sendDataIntent.putExtra("InsulinTiming", insulinTimingSpinner.getSelectedItem().toString());
+                sendDataIntent.putExtra("InsulinTaken", totalBolus);
+
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+                String currentDateTime = dateFormat.format(new Date());
+                sendDataIntent.putExtra("CurrentDateTime", currentDateTime);
+
+                startActivity(sendDataIntent);
                 finish();
+                */
+
             } else {
-                Toast.makeText(BolusCalculator.this, "Error: Please fill in all fields correctly", Toast.LENGTH_LONG).show();
+                Toast.makeText(BolusCalculator.this, "Error: Please fill in all information correctly", Toast.LENGTH_LONG).show();
             }
         });
     }
 
+    private void setupSpinner() {
+        String[] insulinTimings = {"Select Insulin Timing", "Before Breakfast", "After Lunch", "After Dinner"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, insulinTimings) {
+            @Override
+            public boolean isEnabled(int position) {
+                return position != 0;
+            }
+            @Override
+            public View getDropDownView(int position, View convertView, @NonNull ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                tv.setTextColor(position == 0 ? Color.GRAY : Color.BLACK);
+                return view;
+            }
+        };
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        insulinTimingSpinner.setAdapter(adapter);
+    }
+
+    @SuppressLint({"SetTextI18n", "DefaultLocale"})
     private void calculateBolus() {
         try {
             double carbRatio = Double.parseDouble(editTextCarbRatio.getText().toString());
             double glucoseReading = Double.parseDouble(editTextGlucoseReading.getText().toString());
-            double carbsConsumed = totalCarbsConsumed;
 
             if (carbRatio == 0) {
                 textViewResult.setText("Carbohydrate ratio cannot be zero.");
                 return;
             }
 
-            double carbBolus = carbsConsumed / carbRatio;
+            double carbBolus = totalCarbsConsumed / carbRatio;
             double glucoseCorrectionFactor = 50;
             double glucoseCorrectionBolus = (glucoseReading - 120) / glucoseCorrectionFactor;
 
@@ -82,10 +137,9 @@ public class BolusCalculator extends Activity {
                 glucoseCorrectionBolus = 0;
             }
 
-            double totalBolus = carbBolus + glucoseCorrectionBolus;
-            double roundedBolus = roundBolus(totalBolus);
-
-            textViewResult.setText(String.format("Total Insulin Bolus: %.1f units", roundedBolus));
+            totalBolus = carbBolus + glucoseCorrectionBolus;
+            totalBolus = roundBolus(totalBolus);
+            textViewResult.setText(String.format("Total Insulin Bolus: %.1f units", totalBolus));
         } catch (NumberFormatException e) {
             textViewResult.setText("Invalid input");
         }
@@ -104,12 +158,7 @@ public class BolusCalculator extends Activity {
 
     private double roundBolus(double bolus) {
         double fractionalPart = bolus - (int) bolus;
-        if (fractionalPart < 0.4) {
-            return Math.floor(bolus);
-        } else if (fractionalPart <= 0.7) {
-            return Math.floor(bolus) + 0.5;
-        } else {
-            return Math.ceil(bolus);
-        }
+        return fractionalPart < 0.4 ? Math.floor(bolus) : fractionalPart <= 0.7 ? Math.floor(bolus) + 0.5 : Math.ceil(bolus);
     }
 }
+
